ec97319460e82fd1981474561f6fc58c54ea2f44
dthomas <dthomas@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2012-05-04 18:33:17 +0000

    rwandaprimarycare.  doing check-in of the new trunk, mavenized.

---
+++ omod/src/main/java/org/openmrs/module/rwandaprimarycare/GenerateIdsForOtherLocationsController.java
@@ -0,0 +1,35 @@
+package org.openmrs.module.rwandaprimarycare;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.Location;
+import org.openmrs.api.context.Context;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+@RequestMapping("/module/rwandaprimarycare/bulkGenerateIds")
+public class GenerateIdsForOtherLocationsController {
+	
+	  protected static final Log log = LogFactory.getLog(HomepageController.class);
+	    
+	    @RequestMapping(method = RequestMethod.GET)
+	    public String showGenerateIdsPage(ModelMap model, HttpSession session) throws PrimaryCareException {
+	    	Location loc = (Location) Context.getVolatileUserData(PrimaryCareConstants.VOLATILE_USER_DATA_LOGIN_LOCATION);
+	    	model.addAttribute("userLocation", loc);
+	    	//load the locations listed in the list of Location:FOSA global property
+	    	List<Location> locs = PrimaryCareUtil.getLocationsInRwandaLocationCodesGP();
+	    	model.addAttribute("validIdLocations", locs);
+	    
+	    	return "/module/rwandaprimarycare/bulkGenerateIds";
+	    }
+	    
+}
